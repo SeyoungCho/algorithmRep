3b841f3cb0ec180d892587da055aedcfc92f6adb
SeyoungCho1 <45449387+SeyoungCho@users.noreply.github.com>
2022-02-09 04:40:26 +0000

    2022-02-09

---
+++ Dijkstra/9.py
@@ -0,0 +1,34 @@
+#2022-02-09
+#9
+import heapq
+INF = int(1e9)
+graph = [
+  [],
+  [(2,2), (3,5), (4,1)],
+  [(3,3), (4,2)],
+  [(2,3), (6,5)],
+  [(3,3), (5,1)],
+  [(3,1), (6,2)],
+  []
+]
+distance = [INF] * 7
+
+def dijkstra(start):
+  q = [] 
+  heapq.heappush(q, (0, start))
+  distance[start] = 0
+  while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+      continue
+    for i in graph[now]:
+      cost = dist + i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(q, (cost, i[0]))
+dijkstra(1)
+for i in range(1, len(distance)):
+  if distance[i] == INF:
+    print("Shortest path from 1 to %d is INF"%i)
+  else:
+    print("Shortest path from 1 to %d is %d"%(i, distance[i]))
