681b7c12e7f0316f3bccd7d33b7de331afa78027
SeyoungCho1 <45449387+SeyoungCho@users.noreply.github.com>
2023-03-12 01:47:55 +0000

    2023-03-12

---
+++ CountSort/199.py
@@ -0,0 +1,13 @@
+#2023-03-12
+#199
+target_arr = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+count = [0] * (max(target_arr) + 1)
+
+for i in target_arr:
+	count[i] += 1
+
+for i in range(len(count)):
+	for _ in range(count[i]):
+		print(i, end=' ')
+
+print()
