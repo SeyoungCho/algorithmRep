8b3132a8e9314024fd788439e18fe5c8663ed8c7
SeyoungCho1 <45449387+SeyoungCho@users.noreply.github.com>
2023-03-19 02:35:22 +0000

    2023-03-19

---
+++ BinarySearch/206.py
@@ -0,0 +1,22 @@
+#2023-03-19
+#206
+arr = [10, 7, 11, 3, 6,17, 21, 9, 12]
+
+def binary_search(arr, target, start, end):
+	while start <= end:
+		mid = (start + end) // 2
+		if arr[mid] == target:
+			return mid
+		if arr[mid] < target:
+			start = mid + 1
+		else:
+			end = mid - 1
+	return None
+
+arr.sort()
+target = 17
+idx = binary_search(arr, target, 0, len(arr)-1)
+if idx == None:
+	print(f"Target {target} not found")
+else:
+	print(f"Target {target} found at idx {idx}")
